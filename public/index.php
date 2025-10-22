8886bf415432f95bd8002b266a48b305a3e0fc7f
nevermin9 <chizzza@gmail.com>
2025-10-22 16:40:47 +0200

    finished analys; finished layout;

--- public/index.php
+++ public/index.php
@@ -6,39 +6,10 @@ use App\Schedular;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$data = file_get_contents("https://emojihub.yurace.pro/api/random");
-$data = json_decode($data);
-
-// $fp = stream_socket_client("tls://emojihub.yurace.pro:443", $errno, $errmsg);
-//
-// $new_data = "";
-// $headers = [
-//     "Host" => "emojihub.yurace.pro",
-//     "Accept" => "application/json",
-//     "Connection" => "close",
-// ];
-// $headers = array_map(
-//     fn(string $k, string $v) => "{$k}: {$v}",
-//     array_keys($headers),
-//     array_values($headers),
-// );
-// $headers = implode("\r\n", $headers);
-// $headers .= "\r\n\r\n";
-// if (!$fp) {
-//     echo "$errno : $errmsg";
-// } else {
-//     echo "Socket name (remote): " . stream_socket_get_name($fp, true) . PHP_EOL;
-//     echo "Socket name (local): " . stream_socket_get_name($fp, false) . PHP_EOL;
-//     fwrite($fp, "GET /api/random HTTP/1.1\r\n{$headers}");
-//     while (!feof($fp)) {
-//         $new_data .= fgets($fp, 1024);
-//     }
-//     [$respHeaders, $body] = explode("\r\n\r\n", $new_data);
-//     fclose($fp);
-// }
-// $new_data = json_decode();
-
-// ===========
+function getHtmlCode(object $data)
+{
+    return $data->htmlCode[0];
+}
 
 function fetch(string $url, array $headers, Schedular $schedular)
 {
@@ -55,18 +26,268 @@ function fetch(string $url, array $headers, Schedular $schedular)
     return new Fiber($cb);
 }
 
-$schedular = new Schedular();
+$random = null;
+$categories = null;
+$all = null;
 
-$fiber = fetch("https://emojihub.yurace.pro/api/random", ["Content-Type" => "application/json"], $schedular);
-$fiber->start();
-$schedular->run();
-/** @var HttpResponse */
-$response = $fiber->getReturn();
-var_dump($response);
+$urls = [
+    "random" => "https://emojihub.yurace.pro/api/random",
+    "categories" => "https://emojihub.yurace.pro/api/categories",
+    "all" => "https://emojihub.yurace.pro/api/all",
+];
+
+$totalTime = 0;
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $type = filter_input(INPUT_GET, 'type');
+    $type = $type ?? 'sync';
+
+    if ($type === 'sync') {
+        $startTime = microtime(true);
+        $random = file_get_contents($urls['random']);
+        $categories = file_get_contents($urls['categories']);
+        $all = file_get_contents($urls['all']);
+        $totalTime = microtime(true) - $startTime;
+
+        $random = getHtmlCode(json_decode($random));
+        $categories = json_decode($categories);
+        $all = array_map("getHtmlCode", json_decode($all));
+    } elseif ($type === 'async') {
+        $schedular = new Schedular();
+        $fibers = [];
+        /** @var HttpResponse[] */
+        $responses = [];
+
+        $startTime = microtime(true);
+        foreach ($urls as $name => $url) {
+            $fibers[$name] = fetch($url, ["Content-Type" => "application/json"], $schedular);
+        }
+        foreach ($fibers as $f) $f->start();
+        $schedular->run();
+        foreach ($fibers as $name => $f) {
+            if ($f->isTerminated()) {
+                $responses[$name] = $f->getReturn();
+                unset($fibers[$name]);
+            }
+        }
+        $totalTime = microtime(true) - $startTime;
+
+        $random = getHtmlCode($responses['random']?->toJSON());
+        $categories = $responses['categories']?->toJSON();
+        $all = array_map("getHtmlCode", $responses['all']?->toJSON());
+    }
+}
 ?>
 
-<div>
-    <span style="font-size: 46px">
-        <?php echo $data->htmlCode[0]; ?>
-    </span>
-</div>
+<main>
+    <style>
+    :root {
+        color-scheme: light dark;
+        --color-light-scheme: oklch(20.8% 0.042 265.755);
+        --color-dark-scheme: #E9F1F7;
+        --color: light-dark(var(--app-color-light), var(--app-color-dark));
+    }
+    * {
+        box-sizing: border-box;
+    }
+    html {
+        font-size: 16px;
+        color: var(--color);
+    }
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji';
+    }
+
+    .dashboard {
+        display: flex;
+        gap: 16px;
+    }
+
+    .form-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        margin: 0;
+    }
+
+    fieldset {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        border-radius: 8xp;
+    }
+
+    button {
+        flex: 0 0 auto;
+        height: 40px;
+        width: 100%;
+        padding: 0 16px;
+        font-weight: 500;
+        font-size: 0.875rem;
+        background: none;
+        cursor: pointer;
+        overflow: hidden;
+        outline: 0;
+        border-width: 0;
+        transition: all .2s ease;
+        border-radius: 4px;
+        background-color: rgb(34, 116, 255);
+        color: #E9F1F7;
+    }
+
+    th {
+        text-align: left;
+    }
+    td {
+        padding-left: 8px;
+    }
+    article {
+        padding: 0 16px;
+    }
+    h3 {
+        text-align: center;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+    .categories-list {
+        display: flex;
+        justify-content: center;
+        gap: 8px;
+        flex-wrap: wrap;
+    }
+    .categories-list__item {
+        padding: 8px;
+        border-style: solid;
+        border-width: 1px;
+        border-color: var(--color);
+        border-radius: 8px;
+    }
+    .random-emoji {
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        font-size: 46px;
+    }
+    .all-emojis {
+        display: flex;
+        flex-wrap: wrap;
+        font-size: 24px;
+    }
+
+
+    </style>
+
+    <div class="dashboard">
+        <div class="form-container">
+            <form method="get">
+                <fieldset>
+                    <legend>Choose method of requests: </legend>
+
+                    <label>
+                        <input type="radio" name="type" value="async" <?php echo ($type === 'async' ? 'checked' : ''); ?>/>
+
+                        <span>
+                            Asynchronous
+                        </span>
+                    </label>
+
+                    <label>
+                        <input type="radio" name="type" value="sync" <?php echo ($type === 'sync' ? 'checked' : ''); ?> />
+
+                        <span>
+                            Synchrounous
+                        </span>
+                    </label>
+                </fieldset>
+
+                <button>
+                    Run
+                </button>
+            </form>
+
+        </div>
+
+        <table>
+            <tr>
+                <th>
+                    Method: 
+
+                </th>
+
+                <td>
+                    <?php echo ($type === 'sync' ? 'Synchrounous' : 'Asynchrounous') ?>
+                </td>
+            </tr>
+
+            <tr>
+                <th>
+                    Number of requests:
+                </th>
+
+                <td>
+                    <?php echo count($urls) ?>
+                </td>
+            </tr>
+
+            <tr>
+                <th>
+                    Completed in:
+                </th>
+
+                <td>
+                    <?php echo round($totalTime, 3) . "s"; ?>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <article>
+        <h3>
+            Emoji Categories
+        </h3>
+
+        <ul class="categories-list">
+            <?php foreach ($categories as $cat): ?>
+            <li class="categories-list__item">
+                <?php echo $cat ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </article>
+
+    <article>
+        <h3>
+            Random Emoji
+        </h3>
+
+        <p class="random-emoji">
+
+            <?php echo $random ?>
+        </p>
+
+    </article>
+
+
+    <article>
+        <h3>
+            All emojis
+        </h3>
+
+        <ul class="all-emojis">
+            <?php foreach ($all as $e): ?>
+            <li>
+                <?php echo $e ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+    </article>
+</main>
--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -5,23 +5,84 @@ namespace App;
 
 class HttpResponse
 {
-    public readonly string $body;
-    public readonly string $headers;
+    public readonly array $headers;
+    protected string $headersStr;
+    protected string $body;
 
-    public function __construct(public readonly string $response)
+    public function __construct(string $response)
     {
-        [$h, $b] = explode("\r\n\r\n", $this->response);
-        $this->headers = $h;
-        $this->body = $b;
+        $parts = explode("\r\n\r\n", $response, 2); 
+        
+        if (count($parts) < 2) {
+            $this->headersStr = $response;
+            $this->body = '';
+        } else {
+            [$h, $b] = $parts;
+            $this->headersStr = $h;
+            $this->body = $b;
+        }
+
+        // ["Accept: */*", "Host: example.com"]
+        $h = array_slice(array_map("trim", explode("\r\n", $this->headersStr)), 1);
+        $this->headers = array_reduce($h, function ($acc, $current) {
+            [$header, $value] = explode(": ", $current);
+            $acc[$header] = trim($value);
+            return $acc;
+        }, []);
+
+        if ($this->isChunked()) {
+            $this->body = $this->decodeChunkedBody($this->body);
+        }
     }
 
-    public function toJSON()
+    protected function isChunked(): bool
+    {
+        $h = array_find_key($this->headers, fn($v, $k) => strcasecmp($k, 'Transfer-Encoding') === 0);
+        if ($h) {
+            return strpos($this->headers[$h], 'chunked') !== false;
+        }
+        return false;
+    }
+    
+    private function decodeChunkedBody(string $body): string
+    {
+        if (function_exists('http_chunked_decode')) {
+             return http_chunked_decode($body);
+        }
+        
+        $decoded = '';
+        $i = 0;
+        $len = strlen($body);
+
+        while ($i < $len) {
+            if (false === $eol = strpos($body, "\r\n", $i)) {
+                break;
+            }
+            
+            $chunkLengthHex = trim(substr($body, $i, $eol - $i));
+            $chunkLength = hexdec($chunkLengthHex);
+
+            $i = $eol + 2;
+
+            if ($chunkLength === 0) {
+                break;
+            }
+            
+            $decoded .= substr($body, $i, $chunkLength);
+            
+            $i += $chunkLength + 2; 
+        }
+
+        return $decoded;
+    }
+
+    public function toJSON(): mixed
     {
         return json_decode($this->body);
     }
 
     public function __toString()
     {
-        return $this->response;
+        return implode("\r\n\r\n", [$this->headersStr, $this->body]);
     }
 }
